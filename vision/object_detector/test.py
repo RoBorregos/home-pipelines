1845b35dca901c3b5996832128b5ab76578e1151
GilMM27 <A00839075@tec.mx>
2025-05-01 13:51:09 -0600

    changes

--- vision/object_detector/test.py
+++ vision/object_detector/test.py
@@ -1,7 +1,43 @@
+import os
+from PIL import Image
+from PIL import UnidentifiedImageError
+# base_path = "test2"
+# size = 640
+
+# for filename in os.listdir(base_path):
+#     file_path = os.path.join(base_path, filename)
+
+#     try:
+#         with Image.open(file_path) as img:
+#             img = img.resize((size, size))
+#             img.save(file_path)
+#     except (UnidentifiedImageError, OSError) as e:
+#         print(f"Removing corrupt file: {file_path}")
+#         os.remove(file_path)  # delete corrupt image
+
+# angle = -90
+
+# def rotate_images_in_directory(directory):
+#     count = 0
+#     for filename in os.listdir(directory):
+#         filepath = os.path.join(directory, filename)
+#         if os.path.isfile(filepath):
+#             try:
+#                 with Image.open(filepath) as img:
+#                     rotated = img.rotate(angle, expand=True)  # Rotate right (clockwise)
+#                     rotated.save(filepath)
+#                     count += 1
+#             except Exception as e:
+#                 print(f"Failed to process {filename}: {e}")
+#     print(f"Rotated {count} images in {directory}")
+
+# # Example usage:
+# rotate_images_in_directory("test2")
+
 # Test the model
 from ultralytics import YOLO
 # Load the best trained model
-model = YOLO("content/runs/detect/train/weights/epoch75.pt")
+model = YOLO("content/runs/detect/train11/weights/best.pt")
 
 # Run inference on test images
-results = model.predict(source="content/test", save=True, conf=0.5)
+results = model.predict(source="test2", save=True, conf=0.5)
